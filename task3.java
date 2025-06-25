1b6c2391b4f0ecaeda54ef248475130205ce749a
GajaraoNiharika <132187147+GajaraoNiharika@users.noreply.github.com>
2025-06-25 19:48:06 +0530

    Update task3.java

--- task3.java
+++ task3.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class task3 {
     public static void main(String[] args) {
-        double val1=30d;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the no of pounds:");
+        double val1=sc.nextDouble();
         double res=val1*0.45359237d;
         System.out.println("the value in kgs is:"+res);
 
