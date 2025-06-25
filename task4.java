528ed64851f9cba241a0beefdb44d7f8e2a091c7
GajaraoNiharika <132187147+GajaraoNiharika@users.noreply.github.com>
2025-06-25 19:47:37 +0530

    Update task4.java

--- task4.java
+++ task4.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 public class task4 {
     public static void main(String[] args) {
-        byte b=10;
-        short s=20;
-        int i=50;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the byte value:");
+        byte b=sc.nextByte();
+        System.out.println("enter the short:");
+        short s=sc.nextShort();
+        System.out.println("enter the integer:");
+        int i=sc.nextInt();
         long l=50000+10*(b+s+i);
         System.out.println("the long value is :"+l);
     }
