e4c1ef6e54fe1b7c634f3f5cd1a6f3f19722b905
GajaraoNiharika <132187147+GajaraoNiharika@users.noreply.github.com>
2025-06-25 19:48:59 +0530

    Update task2.java

--- task2.java
+++ task2.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 public class task2 {
     public static void main(String[] args) {
-        int score=100000;
-        int levelcompleted=8;
-        int bonus=100;
-        boolean gameover=true;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the score:");
+        int score=sc.nextInt();
+        System.out.println("enter the numberr of levels completed:");
+        int levelcompleted=sc.nextInt();
+        System.out.println("enter the bnous");
+        int bonus=sc.nextInt();
+        System.out.println("game over or not:");
+        boolean gameover=sc.nextBoolean();
         int finalscore=score;
         if (gameover){
             finalscore+=(levelcompleted*bonus);
