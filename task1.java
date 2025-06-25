652befc2631cab130d776542b995c2c7692c2dad
GajaraoNiharika <132187147+GajaraoNiharika@users.noreply.github.com>
2025-06-25 19:38:07 +0530

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class task1 {
     public static void main(String[] args) {
-        double a1=20.00d;
-        double a2=80.00d;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the first number:");
+        double a1=sc.nextDouble();
+        System.out.println("enter the second number:");
+        double a2=sc.nextDouble();
+        
+        
         double val=(a1+a2)*100.00d;
         double remain=val%40.00d;
         System.out.println(remain);
